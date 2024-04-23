f31f06c7aeeabdb515fc3b8a4d37bdf77c6534a1
Serag-Amin <115029341+Serag-Amin@users.noreply.github.com>
2024-04-23 20:05:18 +0000

    modified:   word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -9,19 +9,20 @@ with open('paragraphs.txt', 'r', encoding='utf-8') as file:
 
 # Tokenize the text into words
 words = nltk.word_tokenize(text)
+symbols = ['.', ',', ';', ':', '!', '?', '-', '"', "'", '(', ')', '[', ']', '{', '}', '<', '>', '/', '\\', '|', '@', '#', '$', '%', '^', '&', '*', '_', "'s", "''","``"]
 
 # Remove stop words
 stop_words = set(stopwords.words('english'))
-filtered_words = [word.lower() for word in words if word.lower() not in stop_words]
+filtered_words = [word.lower() for word in words if word.lower() not in stop_words and word not in symbols]
 
 # Count word frequency
 word_count = Counter(filtered_words)
 
-# Get the 15 most common words
+# Get the the top 15 most common words
 most_common_words = word_count.most_common(15)
 
 # Print the most common words
 print("Top 15 Most Common Words:")
 for word, count in most_common_words:
-    print(f"{word}: {count}")
+    print(f"[{word}]: {count}")
 
